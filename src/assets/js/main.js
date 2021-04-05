5327c07bb17e36607ca65355ab5b33e18ac24e0a
mlherve <78431363+mlherve@users.noreply.github.com>
2021-04-05 15:49:13 +0200

    Add files via upload

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,167 +1,132 @@
-$(document).ready(function () {
-  // Menu burger
-  function parrentToggleActive(e, element) {
+//TIMER SECTION BUY
+//Pass database values to s,m,h and the timer will start from there
+var s = 35;
+var m = 20;
+var h = 10;
+document.getElementById("hr").innerHTML = h;
+document.getElementById("min").innerHTML = m;
+document.getElementById("sec").innerHTML = s;
+
+function pad(val) {
+  return val > 9 ? val : "0" + val;
+}
+var seconds = setInterval(function () {
+  document.getElementById("sec").innerHTML = pad(++s);
+  if (s == 60) {
+    document.getElementById("min").innerHTML = pad(++m);
+    s = 0;
+    if (m == 60) {
+      document.getElementById("hr").innerHTML = pad(++h);
+      m = 0;
+    }
+  }
+}, 1000);
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    // Show backToTopButton
+    if (!backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnExit");
+      backToTopButton.classList.add("btnEntrance");
+      backToTopButton.style.display = "block";
+    }
+  } else {
+    // Hide backToTopButton
+    if (backToTopButton.classList.contains("btnEntrance")) {
+      backToTopButton.classList.remove("btnEntrance");
+      backToTopButton.classList.add("btnExit");
+      setTimeout(function () {
+        backToTopButton.style.display = "none";
+      }, 250);
+    }
+  }
+}
+
+//BOUTON BACK TO TOP
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+// function backToTop() {
+//   window.scrollTo(0, 0);
+// }
+
+function smoothScrollBackToTop() {
+  const targetPosition = 0;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  const duration = 750;
+  let start = null;
+
+  window.requestAnimationFrame(step);
+
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    const progress = timestamp - start;
+    window.scrollTo(
+      0,
+      easeInOutCubic(progress, startPosition, distance, duration)
+    );
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
+}
+
+function easeInOutCubic(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return (c / 2) * t * t * t + b;
+  t -= 2;
+  return (c / 2) * (t * t * t + 2) + b;
+}
+
+$(function () {
+  $("a[href*=#]").on("click", function (e) {
     e.preventDefault();
-    e.stopPropagation();
-    element.stop().parent().toggleClass("burger--cross");
-  }
-
-  // Afficher / masquer menu full screen
-  $(".burger").on("click", ".burger__button", function (e) {
-    parrentToggleActive(e, $(this));
-    $(".menu").toggleClass("menu__overlay--active");
-  });
-
-  // Afficher / masquer menu full screen au clic d'un lien
-  $(".menuVertical__nav .nav__anchor").click(function () {
-    $(".burger").removeClass("burger--cross");
-    $(".menu").removeClass("menu__overlay--active");
-  });
-
-  // Apparition des éléments au scroll
-  var sr = ScrollReveal();
-
-  sr.reveal(".title", {
-    delay: 300,
-    origin: "right",
-    distance: "300px",
-  });
-  sr.reveal(".skill", {
-    interval: 300,
-  });
-  sr.reveal(".contact__image", {
-    delay: 300,
-    scale: 0.85,
-  });
-  sr.reveal(".contact__form", {
-    delay: 300,
-    origin: "left",
-    distance: "300px",
-  });
-
-  // Carrousel infini logos compétences
-  $(".skills__technos").slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: !0,
-    autoplaySpeed: 750,
-    arrows: !1,
-    dots: !1,
-    pauseOnHover: !0,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 5,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 4,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
-  });
-
-  // Galerie des réalisations
-  $(".gallery").isotope({
-    itemSelector: ".gallery__item",
-    layoutMode: "fitRows",
-  });
-
-  $(".productions__item").click(function () {
-    $(".productions__item").removeClass("productions__item--active");
-    $(this).addClass("productions__item--active");
-
-    var selector = $(this).attr("data-filter");
-    $(".gallery").isotope({
-      filter: selector,
-    });
-  });
-
-  // Infos projets
-  var nbProductions = $(".gallery").children().length;
-
-  for (var i = 1; i <= nbProductions; i++) {
-    $(".gallery__open-" + i).click(function () {
-      var itemProduction = $(this).parent().next();
-      itemProduction.addClass("work--active");
-    });
-  }
-
-  $(".work__item--close").click(function () {
-    $(".work--close").removeClass("work--active");
-  });
-
-  // Diminuer la taille de police
-  $(".moins").click(function (e) {
-    var taillePolice = $("html").css("font-size");
-    taillePolice = parseInt(taillePolice);
-    if (taillePolice >= 14) {
-      taillePolice = taillePolice - 2;
-      $("html").css("font-size", taillePolice + "px");
-    } else {
-      alert("Vous avez atteint la taille minimale.");
-    }
-  });
-
-  // Augmenter la taille de police
-  $(".plus").click(function (e) {
-    var taillePolice = $("html").css("font-size");
-    taillePolice = parseInt(taillePolice);
-    if (taillePolice <= 20) {
-      taillePolice = taillePolice + 2;
-      $("html").css("font-size", taillePolice + "px");
-    } else {
-      alert("Vous avez atteint la taille maximale.");
-    }
-  });
-
-  // Mode dyslexique
-  $(".dyslexie").click(function (e) {
-    $("*").not("i").toggleClass("dyslexic");
-  });
-
-  // Haut de page
-  var progressPath = document.querySelector(".arrow__circle--shape");
-  var pathLength = progressPath.getTotalLength();
-  progressPath.style.transition = progressPath.style.WebkitTransition = "none";
-  progressPath.style.strokeDasharray = pathLength + " " + pathLength;
-  progressPath.style.strokeDashoffset = pathLength;
-  progressPath.getBoundingClientRect();
-  progressPath.style.transition = progressPath.style.WebkitTransition =
-    "stroke-dashoffset 10ms linear";
-  var updateProgress = function () {
-    var scroll = $(window).scrollTop();
-    var height = $(document).height() - $(window).height();
-    var progress = pathLength - (scroll * pathLength) / height;
-    progressPath.style.strokeDashoffset = progress;
-  };
-
-  updateProgress();
-
-  $(window).scroll(updateProgress);
-  var offset = 50;
-  var duration = 550;
-
-  jQuery(window).on("scroll", function () {
-    if (jQuery(this).scrollTop() > offset) {
-      jQuery(".arrow").addClass("arrow__circle");
-    } else {
-      jQuery(".arrow").removeClass("arrow__circle");
-    }
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
   });
 });
+
+// product Gallery and Zoom
+
+// activation carousel plugin
+var galleryThumbs = new Swiper(".gallery-thumbs", {
+  spaceBetween: 5,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
+});
+var galleryTop = new Swiper(".gallery-top", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
+// change carousel item height
+// gallery-top
+let productCarouselTopWidth = $(".gallery-top").outerWidth();
+$(".gallery-top").css("height", productCarouselTopWidth);
+
+// gallery-thumbs
+let productCarouselThumbsItemWith = $(
+  ".gallery-thumbs .swiper-slide"
+).outerWidth();
+$(".gallery-thumbs").css("height", productCarouselThumbsItemWith);
+
+// activation zoom plugin
+var $easyzoom = $(".easyzoom").easyZoom();
